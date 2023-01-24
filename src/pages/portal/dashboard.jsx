a62b3a82fe3221908c75b27a7bfc30e4af59b709
PrgDominique <dominiquesimbillo10@gmail.com>
2023-01-24 15:19:51 +0800

    added button in dashboard

--- src/pages/portal/dashboard.jsx
+++ src/pages/portal/dashboard.jsx
@@ -1,5 +1,11 @@
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <>
+    <div className="m-5">
+      <button className='rounded-full bg-slate-700 text-2xl'>Time In</button>
+    </div>
+    </>
+  )
 }
 
 export default Dashboard
