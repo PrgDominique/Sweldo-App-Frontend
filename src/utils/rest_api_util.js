2fae5bdab744491b37f96c53905552fd2e0c43e8
jasonlerit <jasonlerit077@gmail.com>
2023-01-24 19:58:20 +0800

    updated rest api util

--- src/utils/rest_api_util.js
+++ src/utils/rest_api_util.js
@@ -12,17 +12,22 @@ const getHeaders = () => {
     return headers
 }
 
-// Auth
+/* 
+    Auth routes
+*/
 export const register = async (data) => await fetch(baseUrl + "/register", { method: "POST", headers: getHeaders(), body: JSON.stringify(data) })
 export const login = async (data) => await fetch(baseUrl + "/login", { method: "POST", headers: getHeaders(), body: JSON.stringify(data) })
 export const forgotPassword = async (data) => await fetch(baseUrl + "/forgot-password", { method: "POST", headers: getHeaders(), body: JSON.stringify(data) })
 export const resetPassword = async (data) => await fetch(baseUrl + "/reset-password", { method: "POST", headers: getHeaders(), body: JSON.stringify(data) })
 
-// Portal
+/* 
+    Authenticated routes
+*/
 export const getDashboard = async () => await fetch(baseUrl + "/dashboard", { method: "GET", headers: getHeaders() })
-
-//clock in 
 export const clockIn = async (data) => await fetch(baseUrl + "/dashboard/clock-in", { method: "POST", headers: getHeaders() })
-
-//clock out
 export const clockOut = async (data) => await fetch(baseUrl + "/dashboard/clock-out", { method: "POST", headers: getHeaders() })
+
+/*
+    Admin routes
+*/
+export const getAdminDashboard = async () => await fetch(baseUrl + "/admin", { method: "GET", headers: getHeaders() })
