9d04ac133720e54c53b15122f28d2bf7ed96f84f
januspaul <januspaul52@gmail.com>
2023-01-25 20:32:06 +0800

    added my-sweldo layout

--- src/pages/portal/user/my-sweldo.jsx
+++ src/pages/portal/user/my-sweldo.jsx
@@ -1,5 +1,30 @@
 const MySweldo = () => {
-  return <div>My Sweldo</div>
-}
+  return (
+    <>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-2 gap-4">
+          <div className="col-span-1 border rounded">
+            <p className="text-2xl text-center m-5">Hours worked</p>
+            <div className="grid grid-cols-2 gap-4">
+              <div className="col-span-1 border rounded m-5">
+                <p className="text-2xl text-center m-5">Weekly</p>
+                <p className="text-7xl text-center m-5">54</p>
+              </div>
+              <div className="col-span-1 border rounded m-5">
+                <p className="text-xl text-center m-5">Montly</p>
+                <p className="text-7xl text-center m-5">452</p>
+              </div>
+            </div>
+            <p className="text-2xl text-center m-5">Employee Rate</p>
+            
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-4">
+          {/* Announcements */}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default MySweldo
+export default MySweldo;
