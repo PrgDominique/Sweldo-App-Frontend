c384e323313015ea62ded4460c9e08cc09f3ad44
PrgDominique <dominiquesimbillo10@gmail.com>
2023-01-25 22:54:14 +0800

    update rest api

--- src/components/portal/user/dashboard/Announcement.jsx
+++ src/components/portal/user/dashboard/Announcement.jsx
@@ -1,37 +1,48 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router'
+import * as RestApi from '../../../../utils/rest_api_util'
 
 const Announcement = () => {
-    return (
-        <>
-        <div className="">
-        <h1 className="mb-8 font-bold">Announcement</h1>
+  const [announcement, setAnnouncement] = useState([])
+
+  const navigate = useNavigate()
+
+  //fetch the announcement from the database
+
+  useEffect(() => {
+    getAnnouncement()
+    // eslint-disable-next-line
+  }, [])
+
+  const getAnnouncement = async () => {
+    try {
+      const result = await RestApi.getAnnouncement()
+      const response = await result.json()
+      if (result.status === 200) {
+        setAnnouncement(response)
+      }
+      if (result.status === 401) {
+        localStorage.clear()
+        navigate('/')
+      }
+    } catch (error) {}
+  }
+
+  return (
+    <>
+      <div className=''>
+        <h1 className='mb-8 font-bold'>Announcement</h1>
         <hr className='mb-2' />
-        <h1 className="mb-8 ">Lorem ipsum dolor sit </h1>
-        <hr className='mb-2' />
-        <h1 className="mb-8">Lorem ipsum dolor sit </h1>
-        <h1 className="mb-8">Lorem ipsum dolor sit </h1>
-        <h1 className="mb-8">Lorem ipsum dolor sit </h1>
+        {announcement.map((item) => (
+          <>
+            <h1 className='mb-8' key={item.id}>{item.title} </h1>
+            <hr className='mb-2' />
+          </>
+        ))}
 
-        <h1 className="mb-8">Lorem ipsum dolor sit </h1>
-        <h1 className="mb-8">Lorem ipsum dolor sit </h1>
-        <h1 className="mb-8">Lorem ipsum dolor sit </h1>
-        <h1 className="mb-8">Lorem ipsum dolor sit </h1>
-        <h1 className="mb-8">Lorem ipsum dolor sit </h1>
-
-        <h1 className="mb-8">Lorem ipsum dolor sit </h1>
-        <h1 className="mb-8">Lorem ipsum dolor sit </h1>
-        <h1 className="mb-8">Lorem ipsum dolor sit </h1>
-        <h1 className="mb-8">Lorem ipsum dolor sit </h1>
-        <h1 className="mb-8">Lorem ipsum dolor sit </h1>
-
-        <h1 className="mb-8">Lorem ipsum dolor sit </h1>
-
-        
-
-       
-
-        </div>
-        </>
-    )
+      </div>
+    </>
+  )
 }
 
 export default Announcement
