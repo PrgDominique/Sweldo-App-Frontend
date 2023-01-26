05610a9997208c025d81d5aaaf7d73d86cc7d7f8
jasonlerit <jasonlerit077@gmail.com>
2023-01-26 13:03:49 +0800

    updated calendar form

--- src/components/portal/user/calendar/CalendarForm.jsx
+++ src/components/portal/user/calendar/CalendarForm.jsx
@@ -1,10 +1,25 @@
 import { Calendar } from 'react-calendar'
 import './CalendarForm.css'
 import * as RestApi from '../../../../utils/rest_api_util'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const CalendarForm = () => {
+  const [monthlyTasks, setMonthlyTasks] = useState([])
   const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    getMonthlyTasks()
+  }, [])
+
+  const getMonthlyTasks = async () => {
+    try {
+      const result = await RestApi.getMonthlyTasks()
+      const response = await result.json()
+      if (result.status === 200) {
+        setMonthlyTasks(response.tasks)
+      }
+    } catch (error) {}
+  }
 
   const onChange = async (value) => {
     try {
@@ -18,14 +33,27 @@ const CalendarForm = () => {
 
   return (
     <div>
-      <Calendar onChange={onChange} />
+      <Calendar
+        onChange={onChange}
+        tileContent={({ activeStartDate, date, view }) => {
+          return view === 'month' &&
+            date.getMonth() === new Date().getMonth() &&
+            monthlyTasks[date.getDate()] !== undefined ? (
+            <div className='mt-1 flex justify-center'>
+              <div className='bg-blue-100 text-blue-800 text-xs font-medium p-1 rounded'>
+                {monthlyTasks[date.getDate()]} task(s)
+              </div>
+            </div>
+          ) : null
+        }}
+      />
       <div className='space-y-5 pt-5'>
         <h1 className='text-2xl'>Task List</h1>
-        {
-          tasks.map((task) => (
-            <div key={task.id} className='bg-white rounded shadow-md p-5'>{task.name}</div>
-          ))
-        }
+        {tasks.map((task) => (
+          <div key={task.id} className='bg-white rounded shadow-md p-5'>
+            {task.name}
+          </div>
+        ))}
       </div>
     </div>
   )
