edc4eaed8949587f256917d1ea42ecaf23f0af01
jasonlerit <jasonlerit077@gmail.com>
2023-01-27 20:16:06 +0800

    updated employee page

--- src/pages/portal/admin/employee.jsx
+++ src/pages/portal/admin/employee.jsx
@@ -1,4 +1,31 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import * as RestApi from '../../../utils/rest_api_util'
+
 const EmployeePage = () => {
+  const navigate = useNavigate()
+
+  const [employees, setEmployees] = useState()
+
+  useEffect(() => {
+    getEmployees()
+    // eslint-disable-next-line
+  }, [])
+
+  const getEmployees = async (page = 1) => {
+    try {
+      const result = await RestApi.getEmployees(page)
+      const response = await result.json()
+      if (result.status === 200) {
+        setEmployees(response.employees)
+      }
+      if (result.status === 401) {
+        localStorage.clear()
+        navigate('/')
+      }
+    } catch (error) {}
+  }
+
   return (
     <div>
       <div>Employee Page</div>
@@ -14,24 +41,18 @@ const EmployeePage = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className='border-b'>
-              <th className='p-2.5'>1</th>
-              <td className='p-2.5'>Jason</td>
-              <td className='p-2.5'>Lerit</td>
-              <td className='p-2.5'>jason@gmail.com awdawdwd</td>
-              <td className='p-2.5'>
-                <button>Edit</button>
-              </td>
-            </tr>
-            <tr className='border-b'>
-              <th className='p-2.5'>1</th>
-              <td className='p-2.5'>Jason</td>
-              <td className='p-2.5'>Lerit</td>
-              <td className='p-2.5'>jason@gmail.com awdawdwd</td>
-              <td className='p-2.5'>
-                <button>Edit</button>
-              </td>
-            </tr>
+            {employees !== undefined &&
+              employees.data.map((employee, index) => (
+                <tr key={index} className='border-b'>
+                  <th className='p-2.5'>{employee.id}</th>
+                  <td className='p-2.5'>{employee.first_name}</td>
+                  <td className='p-2.5'>{employee.last_name}</td>
+                  <td className='p-2.5'>{employee.email}</td>
+                  <td className='p-2.5'>
+                    <button>Edit</button>
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
