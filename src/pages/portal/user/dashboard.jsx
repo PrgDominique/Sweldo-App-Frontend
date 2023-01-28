6f87514ac2626220506caee9fd4aad000c2e7380
PrgDominique <dominiquesimbillo10@gmail.com>
2023-01-28 13:44:00 +0800

    update dashboard

--- src/pages/portal/user/dashboard.jsx
+++ src/pages/portal/user/dashboard.jsx
@@ -31,34 +31,31 @@ const Dashboard = () => {
 
   return (
     <>
-      <div className='grid grid-cols-4'>
-        <div className='col-span-3'>
-          <div className='grid grid-cols-3'>
-            <div className='row-span-1'>qwe</div>
+      <div className='grid grid-cols-6'>
+        <div className='col-span-4'>
+          <div className='grid grid-cols-4 gap-5'>
+            <div className='col-span-1 shadow h-24 w-48 justify-center flex items-center'>Weekly</div>
 
-            <div className='row-span-1'>asd</div>
+            <div className='col-span-1 shadow justify-center flex items-center h-24 w-48'>Monthly</div>
 
-            <div className='row-span-1'>zxc</div>
+            <div className='col-span-1 shadow justify-center flex items-center h-24 w-48'>Blank</div>
+            <div className='col-span-1 shadow justify-center flex items-center h-24 w-48'>Expected Salary</div>
           </div>
           
         </div>
-          <div className='col-span-3'>
-            qwe
-          </div>
+     
           
 
-        <div className='col-span-1 gap-12'>
+        <div className='col-span-2 '>
           <div className="grid cols-span-1">
 
-          <h1 className=' flex justify-center font-bold text-xl mb-5'>
+          <h1 className=' flex justify-center font-bold text-xl '>
             Announcement
           </h1>
           <div className='announce overflow-auto mb-5 shadow flex justify-center'>
             <Announcement announcements={announcements} />
           </div>
-          <div className='shoadow mt-6'>
-            <TimeCard isClockIn={isClockIn} />
-          </div>
+          
           </div>
         </div>
       </div>
