963b928a724b7dd74fadf6e4f568401c0b9898bc
PrgDominique <dominiquesimbillo10@gmail.com>
2023-01-28 16:52:15 +0800

    update dashboard

--- src/pages/portal/user/dashboard.jsx
+++ src/pages/portal/user/dashboard.jsx
@@ -34,47 +34,22 @@ const Dashboard = () => {
       <div className='grid grid-cols-1 md:grid-cols-6 gap-5'>
         <div className='md:col-span-4'>
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
-            <div className='bg-white shadow p-5'>1</div>
-            <div className='bg-white shadow p-5'>2</div>
-            <div className='bg-white shadow p-5'>3</div>
-            <div className='bg-white shadow p-5'>4</div>
+            <div className='bg-white shadow p-5'>Weekly</div>
+            <div className='bg-white shadow p-5'>Monthly</div>
+            <div className='bg-white shadow p-5'>Blank</div>
+            <div className='bg-white shadow p-5'>Expected Salary</div>
             <div className='md:col-span-2 lg:col-span-4 bg-white shadow p-5'>Attendance</div>
           </div>
         </div>
-        <div className='md:col-span-2 bg-white shadow p-5'>Announcement</div>
-      </div>
-
-      {/* <div className='grid grid-cols-4'>
-        <div className='col-span-3'>
-          <div className='grid grid-cols-3'>
-            <div className='row-span-1'>qwe</div>
-    <>
-      <div className='grid xl:grid-cols-8 lg:grid-cols-6 md:grid-cols-4 grid-cols-2'>
-        <div className='xl:col-span-6 lg:col-span-4 md:col-span-4 col-span-2'>
-          <div className='grid xl:grid-cols-4 lg:grid-cols-2 md:grid-cols-2 grid-cols-2 gap-5'>
-            <div className='col-span-1 shadow h-24 w-48 justify-center flex items-center'>Weekly</div>
-
-            <div className='col-span-1 shadow justify-center flex items-center h-24 w-48'>Monthly</div>
-
-            <div className='col-span-1 shadow justify-center flex items-center h-24 w-48'>Blank</div>
-            <div className='col-span-1 shadow justify-center flex items-center h-24 w-48'>Expected Salary</div>
-          </div>
-          
-        </div>
-     
-    
-
-        <div className='xl:col-span-2 lg:col-span-2 col-span-4 mt-11   '>
-
-          <h1 className=' flex justify-center font-bold text-xl '>
+        <div className='md:col-span-2 bg-white shadow p-5'>
+        <h1 className=' flex justify-center font-bold text-xl '>
             Announcement
           </h1>
-          <div className='announce overflow-auto mb-5 shadow flex justify-center'>
             <Announcement announcements={announcements} />
-          
-          </div>
-        </div>
-      </div> */}
+            </div>
+      </div>
+
+    
     </div>
   )
 }
