045689ddee5dd05f93892a864bd50cf11a3f0739
PrgDominique <dominiquesimbillo10@gmail.com>
2023-01-28 09:12:34 +0800

    fix alignment of announcement in dashboard

--- src/pages/portal/user/dashboard.jsx
+++ src/pages/portal/user/dashboard.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Announcement from '../../../components/portal/user/dashboard/Announcement'
-import CalendarWidget from '../../../components/portal/user/dashboard/CalendarWidget'
 import TimeCard from '../../../components/portal/user/dashboard/TimeCard'
 import * as RestApi from '../../../utils/rest_api_util'
 
@@ -33,19 +32,31 @@ const Dashboard = () => {
 
   return (
     <>
+    
+<div className="grid grid-cols-4">
 
-     <div className='w-1/2 p-5 shadow-md lg:max-w-lg  overflow-auto float-right announce flex justify-center'>
-              <Announcement announcements={announcements}/>
+<div className='row-span-1'>
+             
             </div>
-    <div className="flex gap-x-12 ">
 
-          <div className='w-1/4 p-4 shadow-md lg:max-w-lg h-auto flex justify-center'>
+<div className='row-span-1'>
+             
+            </div>
+<div className='row-span-1'>
+             
+            </div>
+
+<div className="col-span-1 ">
+
+          <div className='announce overflow-auto mb-5 shadow '>
+          <Announcement announcements={announcements}/>
+        </div>
+          <div className='shoadow mt-6'>
             <TimeCard isClockIn={isClockIn} />
           </div>
-          <div className='w-1/2 p-4 shadow-md lg:max-w-lg  h-10 flex  justify-center'>
-            <CalendarWidget />
-        </div>
-    </div>
+</div>
+</div>
+
            
     </>
   )
