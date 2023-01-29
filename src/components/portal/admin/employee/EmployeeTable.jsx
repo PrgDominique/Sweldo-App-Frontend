38334601336843b2d4ec95ce0abd4f7956a2d535
jasonlerit <jasonlerit077@gmail.com>
2023-01-29 20:12:16 +0800

    updated employee table

--- src/components/portal/admin/employee/EmployeeTable.jsx
+++ src/components/portal/admin/employee/EmployeeTable.jsx
@@ -19,7 +19,6 @@ const EmployeeTable = ({ employees }) => {
       const response = await result.json()
       if (result.status === 200) {
         setFormData({ ...formData, normal: response.normal })
-        console.log(employee)
         setSelectedEmployee(employee)
       }
       if (result.status === 401) {
@@ -62,7 +61,7 @@ const EmployeeTable = ({ employees }) => {
             (employees.data.length !== 0 ? (
               employees.data.map((employee, index) => (
                 <tr key={index} className='border-b'>
-                  <th className='p-2.5'>{employee.id}</th>
+                  <th className='p-2.5'>{employees.from + index}</th>
                   <td className='p-2.5'>{employee.first_name}</td>
                   <td className='p-2.5'>{employee.last_name}</td>
                   <td className='p-2.5'>{employee.email}</td>
