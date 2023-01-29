3560f2d41fc098520495dbe6b4d65b84df14d149
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-01-29 14:24:16 +0800

    in progress profile employee

--- src/pages/portal/user/profile.jsx
+++ src/pages/portal/user/profile.jsx
@@ -1,15 +1,17 @@
 
 const Profile = () => {
-    return ( 
-    
-    <>
-    <div>Profile Page</div>
+    return (
+        <div className="bg-gray-200 p-5 m-5">
+            <span class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">Active</span>
+            <p className="text-2xl font-bold">IT001</p>
 
-        <div>Employee Name</div>
-        <div>Position</div>
-        
-    </>
-        
+            <p className="text-xl font-bold">John Doe</p>
+
+            <p>Junior Web Developer - IT Department</p>
+            <p>Expected Salary: 15000</p>
+
+        </div>
+
     )
 }
 
